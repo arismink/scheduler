8aed36afcd27b23f322ec22b76c163d1ab0ffc11
Mina <mina@mina-ubuntu.hitronhub.home>
2022-04-19 16:05:01 -0600

    add button styling on storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+
+   const { onClick, disabled, children, confirm, danger } = props;
+
+   const buttonClass = classNames("button", {
+      "button--confirm": confirm,
+      "button--danger": danger
+   })
+
+   return (
+      <button
+         onClick={onClick}
+         className={buttonClass}
+         disabled={disabled}>{children}</button>
+   );
+};
+

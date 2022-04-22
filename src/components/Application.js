d3ee7639e672e85a11b974b12fa2fb8e1decddfd
arismink <krismina.la@gmail.com>
2022-04-21 20:32:46 -0600

    remove hardcoded days array, add useEffect to retrieve days array from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,28 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = {
   "1": {
@@ -64,7 +65,14 @@ const appointments = {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(res => {
+      setDays([...res.data]);
+    })
+  }, []);
 
   const appointmentsArray = Object.values(appointments).map(appointment => {
     return (
@@ -86,9 +94,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-              days={days}
-              value={day}
-              onChange={setDay}
+              days={[days]}
+              value={"Monday"}
+              onChange={setDays}
             />
         </nav>
         <img

b8a1dc1c9f08fce3c26353d0325b2207f3005a23
arismink <krismina.la@gmail.com>
2022-04-24 15:49:16 -0600

    create custom hook taht keeps track of state of appointment component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+export default function useVisualMode(init) {
+  const [mode, setMode] = useState(init);
+
+  const [history, setHistory] = useState([init]);
+
+  // Transition mode to the next mode
+  function transition(newMode, replaced = false) {
+
+    // If replace is true, we are replacing current mode in history
+    if (replaced) setHistory([...history]);
+  
+    // Set new history with what was previously in the history array 
+    else setHistory([...history, newMode]);
+
+    setMode(newMode);
+  }
+  
+  // Transition to previous mode
+  function back() {
+    
+    // Remove last item from history array and ensure that length is always greater than 1
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      history.pop();
+
+      // Ensure that new history is recognized by React so it re-renders
+      setHistory([...history])
+
+      // Back limit
+    } else {
+      setMode(init)
+    }
+  }
+  
+  return { mode, transition, back }
+};

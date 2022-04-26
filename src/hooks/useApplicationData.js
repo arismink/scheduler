ccc4fbfb2870e89697483aa8b5d9aecf93536d10
arismink <krismina.la@gmail.com>
2022-04-26 16:59:56 -0600

    add updateSpots functionality that doesn't mutate state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,32 +2,6 @@ import {useState, useEffect} from 'react';
 
 import axios from 'axios';
 
-
-const updateSpots = (state) => {
-
-  // Find the index of the specific day
-  const dayIndex = state.days.findIndex(d => d.name === state.day)
-
-  // Get array of appointmentIDs for specified day
-  const dayApptSchedule = state.days[dayIndex].appointments;
-
-  // Get array of appointment objects that correspond to the elements in dayApptSchedule
-  const apptArray = Object.values(state.appointments).filter(a => dayApptSchedule.includes(a.id));
-
-  const remainingSpots = apptArray.filter(a => !a.interview).length;
-
-  const day = {
-    ...state.days[dayIndex],
-    spots: remainingSpots
-  };
-
-  const days = [...state.days].splice(dayIndex, 1).push(day);
-
-  return days;
-
-}
-
-
 export default function useApplicationData() {
   const [state, setState] = useState({
     day: "Monday",
@@ -57,6 +31,31 @@ export default function useApplicationData() {
     })
     }, []);
 
+  
+  const updateSpots = (state, appointments) => {
+
+    // Find day obj
+    const dayObj = state.days.find(d => d.name === state.day);
+
+    // Get array of appointmentIDs for specified day
+    const dayApptSchedule = dayObj.appointments;
+
+    // Get array of appointment objects that correspond to the elements in dayApptSchedule
+    const apptArray = Object.values(appointments).filter(a => dayApptSchedule.includes(a.id));
+
+    const remainingSpots = apptArray.filter(a => !a.interview).length;
+
+    // Create new day object
+    const day = {...dayObj, spots: remainingSpots}
+
+    // Create new array that includes the new day
+    const days = state.days.map(d => d.name === state.day ? day : d);
+
+    return days
+  
+  }
+    
+
 
   // Allow change to local state when an interview is booked
   function bookInterview(id, interview) {
@@ -71,14 +70,14 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
-    // Retrieve new array with the updated spots
-    const days = updateSpots(state)
 
-    return axios.put(`/api/appointments/${id}`, appointment)
+    return axios.put(`/api/appointments/${id}`, {interview})
       .then((res) => {
         // Call setState and update it with the newly booked appointment
 
-        console.log('days', days);
+        // Retrieve new array with the updated spots
+        const days = updateSpots(state, appointments);
+
         setState(prev => ({
           ...prev,
           appointments,
@@ -102,11 +101,11 @@ export default function useApplicationData() {
     };
 
 
-    // Retrieve new array with the updated spots
-    const days = updateSpots(state);
-
     return axios.delete(`/api/appointments/${id}`, appointment)
       .then(res => {
+        // Retrieve new array with the updated spots
+        const days = updateSpots(state, appointments);
+
         setState(prev => ({
           ...prev,
           appointments,

91d72ad6e994fe5d1f3a76d3ee10887310f2dc14
arismink <krismina.la@gmail.com>
2022-04-25 23:51:36 -0600

    add error handling

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,7 @@ import Status from './Status';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
 import Confirm from './Confirm';
+import Error from './Error';
 
 import useVisualMode from 'hooks/useVisualMode';
 
@@ -18,30 +19,36 @@ const SAVE = "SAVE";
 const DELETE = "DELETE";
 const CONFIRM= "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE"
 
 export default function Appointment(props) {
 
   function save(name, interviewer) {
     if (!name || !interviewer) return alert('Please enter valid input.');
 
-    transition(SAVE);
+    transition(SAVE, true);
     const interview = {
       student: name,
       interviewer
     };
 
     // When promise returned by axios put request is resolved, transition to SHOW
-    props.bookInterview(props.id, interview)
-      .then(res => transition(SHOW));
+    props
+      .bookInterview(props.id, interview)
+      .then(res => transition(SHOW))
+      .catch(err => transition(ERROR_SAVE, true));
   
   };
 
   // After confirmed, delete
   function _delete() {
-    transition(DELETE);
-    (props.cancelInterview(props.id))
+    transition(DELETE, true);
+
+    props
+      .cancelInterview(props.id)
       .then(res => transition(EMPTY))
-      .catch(err => err.message)
+      .catch(err => transition(ERROR_DELETE, true))
   }
 
   const {mode, transition, back} = useVisualMode(
@@ -89,6 +96,20 @@ export default function Appointment(props) {
           interviewers={props.interviewers}
           onSave={save}
           onCancel={back}
+        />
+      )}
+
+      {mode === ERROR_SAVE && (
+        <Error
+          message="Error saving interview. Please try again."
+          onClose={back}
+        />
+      )}
+
+      {mode === ERROR_DELETE && (
+        <Error
+          message="Error cancelling interview. Please try again."
+          onClose={back}
         />
       )}
       

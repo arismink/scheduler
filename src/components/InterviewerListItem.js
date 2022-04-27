0b2fd79072a5744f05a3f24361976e56e0aa791a
arismink <krismina.la@gmail.com>
2022-04-26 19:36:41 -0600

    remove unused variables

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './InterviewerListItem.scss';
 
 export default function InterviewerListItem(props) {
-  const {id, name, avatar, selected, setInterviewer} = props;
+  const {name, avatar, selected, setInterviewer} = props;
 
   const interviewerClass = classNames('interviewers__item',
     {
